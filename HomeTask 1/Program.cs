c79b02951315b2c6ce15e5990d945af3c90040ca
nihadmm <nihadmm@code.edu.az>
2022-07-31 01:45:22 +0400

    HomeTask 1

---
+++ HomeTask 1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Wellcome to my first hometask.");
+int a = 5;
+int b = 21;
+Console.WriteLine("a ve b deyishenlerinin toplanmasi ve vurulmasi ve bir sira emeller ashagida qeyd olunub");
+Console.WriteLine(a + b);
+Console.WriteLine(a * b);
+Console.WriteLine(b % a);
+a++; b++;
+Console.WriteLine(a + b);
+Console.WriteLine(a < b);
+int c = 16;
+Console.WriteLine(a + c != b);
+int d = 17;
+Console.WriteLine(d > a || c > b);
+Console.WriteLine(b > d && a < c);
+
+Console.WriteLine(!(b > d && a < c));
+d += 5; Console.WriteLine(d);
+c -= 2; Console.WriteLine(c);
+
+Console.WriteLine( );
+string name = "BMW";
+int model = 2017;
+char series = 'M';
+bool isNew = false;
+Console.WriteLine(model+" model "+name+" cerimelendi.");
+
+
